c3d11a7f7ebeadd5d59644442687ced485155c68
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-09-19 12:16:35 +0700

    fix get curencies data use case to support failure scenario

--- lib/domain/usecases/get_currencies_data.dart
+++ lib/domain/usecases/get_currencies_data.dart
@@ -1,12 +1,14 @@
 import 'package:currency_charts/domain/entities/currency.dart';
 import 'package:currency_charts/domain/repositories/currency_repository.dart';
+import 'package:currency_charts/utils/failures.dart';
+import 'package:dartz/dartz.dart';
 
 class GetCurrenciesData {
   final CurrencyRepository repository;
 
   GetCurrenciesData({required this.repository});
 
-  Future<Currency> execute() {
+  Future<Either<Failure, Currency>> execute() {
     return repository.getCurrenciesData();
   }
 }
--- test/domain/usecases/get_currencies_data_test.dart
+++ test/domain/usecases/get_currencies_data_test.dart
@@ -1,6 +1,7 @@
 import 'package:currency_charts/domain/entities/currency.dart';
 import 'package:currency_charts/domain/repositories/currency_repository.dart';
 import 'package:currency_charts/domain/usecases/get_currencies_data.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -15,11 +16,12 @@ void main() {
     // arrange
     final mockRepository = MockCurrencyRepository();
     final usecase = GetCurrenciesData(repository: mockRepository);
-    when(mockRepository.getCurrenciesData()).thenAnswer((_) async => tCurrency);
+    when(mockRepository.getCurrenciesData())
+        .thenAnswer((_) async => Right(tCurrency));
     // act
     final result = await usecase.execute();
     // assert
     verify(mockRepository.getCurrenciesData());
-    expect(result, tCurrency);
+    expect(result, Right(tCurrency));
   });
 }
